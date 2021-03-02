6532ef65d3df26ee1368f01416e8dd61544ed244
mewiemers <m.wiemers01@gmail.com>
2021-03-02 09:53:58 +0100

    💄 add chalk

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,56 @@
-console.log("Hello Mel");
+import prompts from "prompts";
+import chalk from "chalk";
 
-// process.argv.forEach((val, index) => {
-//   if (val === "-v") {
-//     console.log("0.1.0");
-//   }
-//   //   console.log(`${index}: ${val}`);
-// });
+const questions = async () => {
+  console.log(chalk.keyword("violet").underline("Hello Mel"));
 
-// console.log(process.argv.slice(2));
+  const answers = await prompts([
+    {
+      type: "number",
+      name: "age",
+      message: "How old are you?",
+      validate: (value: number) =>
+        value < 18 ? `Sorry, you have to be 18` : true,
+    },
+    {
+      type: "password",
+      name: "secret",
+      message: "Tell me a secret",
+    },
+    {
+      type: "confirm",
+      name: "confirmed",
+      message: "Can you confirm?",
+    },
 
-// const args = process.argv.slice(2)
-// const command = args[0]
+    {
+      type: "list",
+      name: "keywords",
+      message: "Enter keywords",
+    },
+    {
+      type: "select",
+      name: "color",
+      message: "Pick a color",
+      choices: [
+        {
+          title: "Red",
+          description: "This option has a description.",
+          value: "#ff0000",
+        },
+        { title: "Green", value: "#00ff00" },
+        { title: "Yellow", value: "#ffff00", disabled: false },
+        { title: "Blue", value: "#0000ff" },
+      ],
+    },
 
-//destructure
-const [command] = process.argv.slice(2);
-
-if (command === "set") {
-  console.log("set something");
-} else if (command === "get") {
-  console.log("Get something");
-}
+    {
+      type: "date",
+      name: "birthday",
+      message: `What's your birthday?`,
+      validate: (date: number) =>
+        date > Date.now() ? `Your birthday can't be in the future` : true,
+    },
+  ]);
+};
+questions();
