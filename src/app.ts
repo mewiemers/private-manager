e243b826a9cc057e542559cdef5953682bbedbfd
mewiemers <m.wiemers01@gmail.com>
2021-03-02 18:29:58 +0100

    add connection with mongodb

--- src/app.ts
+++ src/app.ts
@@ -5,31 +5,54 @@ import {
   askForPassword,
   chooseColor,
 } from "./questions";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-printHelloMessage();
+// printHelloMessage();
 
 const run = async () => {
-  const age = await askForAge();
-  //   if (!isUnderaged(age)) {
-  //     printUnderaged();
-  //     askfordata();
-  //     return;
-  //   }
-  askForPassword();
-  askConfirm();
-  const choose = await chooseColor();
-  switch (choose.color) {
-    case "#0000ff":
-      "Blue";
-      break;
-    case "#00ff00":
-      "Green";
-      break;
-    case "#ff0000":
-      "Red";
-      break;
-    case "#ffff00":
-      "Yellow";
+  const url = process.env.MONGODB_URL;
+
+  try {
+    const client = await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB!");
+
+    const db = client.db("private-manager-melanie");
+    await db.collection("inventory").insertOne({
+      item: "canvas",
+      qty: 200,
+      tags: ["polyester"],
+      size: { h: 28, w: 35.5, unit: "cm" },
+    });
+    client.close();
+  } catch (error) {
+    console.error(error);
   }
+
+  //   const age = await askForAge();
+  //   //   if (!isUnderaged(age)) {
+  //   //     printUnderaged();
+  //   //     askfordata();
+  //   //     return;
+  //   //   }
+  //   askForPassword();
+  //   askConfirm();
+  //   const choose = await chooseColor();
+  //   switch (choose.color) {
+  //     case "#0000ff":
+  //       "Blue";
+  //       break;
+  //     case "#00ff00":
+  //       "Green";
+  //       break;
+  //     case "#ff0000":
+  //       "Red";
+  //       break;
+  //     case "#ffff00":
+  //       "Yellow";
+  //   }
 };
 run();
