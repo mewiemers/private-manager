e485b5a647183e0e5fa365581a21e6fed9ce083b
mewiemers <m.wiemers01@gmail.com>
2021-03-02 14:41:50 +0100

    outsource code

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,35 @@
-import prompts from "prompts";
-import chalk from "chalk";
+import { printHelloMessage } from "./message";
+import {
+  askForAge,
+  askConfirm,
+  askForPassword,
+  chooseColor,
+} from "./questions";
 
-const questions = async () => {
-  console.log(chalk.keyword("violet").underline("Hello Mel"));
+printHelloMessage();
 
-  const answers = await prompts([
-    {
-      type: "number",
-      name: "age",
-      message: "How old are you?",
-      validate: (value: number) =>
-        value < 18 ? `Sorry, you have to be 18` : true,
-    },
-    {
-      type: "password",
-      name: "secret",
-      message: "Tell me a secret",
-    },
-    {
-      type: "confirm",
-      name: "confirmed",
-      message: "Can you confirm?",
-    },
-
-    {
-      type: "list",
-      name: "keywords",
-      message: "Enter keywords",
-    },
-    {
-      type: "select",
-      name: "color",
-      message: "Pick a color",
-      choices: [
-        {
-          title: "Red",
-          description: "This option has a description.",
-          value: "#ff0000",
-        },
-        { title: "Green", value: "#00ff00" },
-        { title: "Yellow", value: "#ffff00", disabled: false },
-        { title: "Blue", value: "#0000ff" },
-      ],
-    },
-
-    {
-      type: "date",
-      name: "birthday",
-      message: `What's your birthday?`,
-      validate: (date: number) =>
-        date > Date.now() ? `Your birthday can't be in the future` : true,
-    },
-  ]);
+const run = async () => {
+  const age = await askForAge();
+  //   if (!isUnderaged(age)) {
+  //     printUnderaged();
+  //     askfordata();
+  //     return;
+  //   }
+  askForPassword();
+  askConfirm();
+  const choose = await chooseColor();
+  switch (choose.color) {
+    case "#0000ff":
+      "Blue";
+      break;
+    case "#00ff00":
+      "Green";
+      break;
+    case "#ff0000":
+      "Red";
+      break;
+    case "#ffff00":
+      "Yellow";
+  }
 };
-questions();
+run();
