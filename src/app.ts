f4f1577abbae99e87206cff0e1fbaf559a12ff4d
mewiemers <m.wiemers01@gmail.com>
2021-03-02 22:23:37 +0100

    changes db

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,59 @@
-import { printHelloMessage } from "./message";
+import { printHelloMessage, printUnderaged } from "./message";
 import {
   askForAge,
   askConfirm,
   askForPassword,
   chooseColor,
 } from "./questions";
+import { isUnderaged } from "./command";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-// printHelloMessage();
+printHelloMessage();
 
 const run = async () => {
-  const url = process.env.MONGODB_URL;
+  //   const url = process.env.MONGODB_URL;
 
-  try {
-    const client = await MongoClient.connect(url, {
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to DB!");
+  //   try {
+  //     const client = await MongoClient.connect(url, {
+  //       useUnifiedTopology: true,
+  //     });
+  //     console.log("Connected to DB!");
 
-    const db = client.db("private-manager-melanie");
-    await db.collection("inventory").insertOne({
-      item: "canvas",
-      qty: 200,
-      tags: ["polyester"],
-      size: { h: 28, w: 35.5, unit: "cm" },
-    });
-    client.close();
-  } catch (error) {
-    console.error(error);
-  }
-
-  //   const age = await askForAge();
-  //   //   if (!isUnderaged(age)) {
-  //   //     printUnderaged();
-  //   //     askfordata();
-  //   //     return;
-  //   //   }
-  //   askForPassword();
-  //   askConfirm();
-  //   const choose = await chooseColor();
-  //   switch (choose.color) {
-  //     case "#0000ff":
-  //       "Blue";
-  //       break;
-  //     case "#00ff00":
-  //       "Green";
-  //       break;
-  //     case "#ff0000":
-  //       "Red";
-  //       break;
-  //     case "#ffff00":
-  //       "Yellow";
+  //     const db = client.db("private-manager-melanie");
+  //     await db.collection("inventory").insertOne({
+  //       item: "canvas",
+  //       qty: 200,
+  //       tags: ["polyester"],
+  //       size: { h: 28, w: 35.5, unit: "cm" },
+  //     });
+  //     client.close();
+  //   } catch (error) {
+  //     console.error(error);
   //   }
+
+  const age = await askForAge();
+  if (!isUnderaged(age)) {
+    printUnderaged();
+    run();
+    return;
+  }
+  await askForPassword();
+  await askConfirm();
+  const choose = await chooseColor();
+  switch (choose.color) {
+    case "#0000ff":
+      "Blue";
+      break;
+    case "#00ff00":
+      "Green";
+      break;
+    case "#ff0000":
+      "Red";
+      break;
+    case "#ffff00":
+      "Yellow";
+  }
 };
 run();
--- src/message.ts
+++ src/message.ts
@@ -3,3 +3,7 @@ import chalk from "chalk";
 export const printHelloMessage = () => {
   console.log(chalk.keyword("violet").underline("Hello Mel"));
 };
+
+export const printUnderaged = () => {
+  console.log(chalk.redBright("Sorry you are too young"));
+};
